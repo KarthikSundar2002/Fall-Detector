eb0cbbba3584c0b7e25102fcb6f219541f211301
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2024-03-27 15:08:35 +0530

    feat: Video Output

--- helpers.py
+++ helpers.py
@@ -1,4 +1,17 @@
+import cv2
+import os
+import re
+from natsort import os_sorted
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
 def descentSpeedCheck(prev_hip_center, hip_center, fps, threshold=0.05):
+    if prev_hip_center is None or hip_center is None:
+        return False
     delta_t = 5/fps 
     vertical_vel = (prev_hip_center[1] - hip_center[1])/delta_t
     if vertical_vel >= threshold:
@@ -7,8 +20,22 @@ def descentSpeedCheck(prev_hip_center, hip_center, fps, threshold=0.05):
         return False
     
 def angleCenterlineCheck(prev_angle, angle, threshold=45):
+    if prev_angle is None or angle is None:
+        return False
     delta_angle = abs(prev_angle - angle)
     if delta_angle >= threshold:
         return True
     else:
         return False
+
+def compileToVideo(video_path,output_path,flag, height,width,fps):
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+    frames = os_sorted(os.listdir(video_path))
+    for frame in frames:
+        frame = cv2.imread(video_path + str(frame))
+        if flag:
+            frame = cv2.putText(frame, "Fall Detected", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2
+        else:
+            frame = cv2.putText(frame, "No Fall Detected", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        out.write(frame)
--- main.py
+++ main.py
@@ -6,54 +6,64 @@ import numpy as np
 from helpers import *
 
 model = YOLO('yolov8n-pose.pt')
+video_path = './videos/4.mp4'
 
-video_cap = cv2.VideoCapture('./videos/fall-01-cam0.mp4')
+video_cap = cv2.VideoCapture(video_path)
 fps = video_cap.get(cv2.CAP_PROP_FPS)
 success, frame = video_cap.read()
 (height, width) = frame.shape[:2]
-print(height, width)
-count = 0
-while success:
-    if count%5 == 0:
-        frame = frame[0:240, 320:640]
-        cv2.imwrite(f"imgs/frame_{count}.jpg", frame)
-    success, frame = video_cap.read()
-    count += 1
+# print(height, width)
+# count = 0
+# while success:
+#     if count%5 == 0:
+#         frame = frame[0:240, 320:640]
+#         cv2.imwrite(f"imgs/frame_{count}.jpg", frame)
+#     success, frame = video_cap.read()
+#     count += 1
 
-frames = list(pathlib.Path('imgs').glob('*.jpg'))
+# frames = list(pathlib.Path('imgs').glob('*.jpg'))
 
-results = model(frames)
+results = model(video_path,stream=True)
 i  = 0
 prev_hip_center = None
 prev_angle = None
+flag = False
 for result in results:
     keypoints = result.keypoints.xyn
-    if keypoints.shape != (1,17,2):
+    print(keypoints.shape)
+    if keypoints.shape[1] != 17 or keypoints.shape[2] != 2:
+        result.save(f'intermediate/frame_{i}.jpg')
         i += 1
         continue
-    right_hip = keypoints[0][8]
-    left_hip = keypoints[0][11]
-    hip_center = (right_hip + left_hip) / 2
-    nose = keypoints[0][0]
-    right_foot = keypoints[0][10]
-    left_foot = keypoints[0][13]
-    mean_foot = (right_foot + left_foot) / 2
-    nose = nose.cpu().numpy()
-    mean_foot = mean_foot.cpu().numpy()
-    if nose[0] != mean_foot[0]:
-        angle = np.arctan((nose[1]-mean_foot[1])/(nose[0] - mean_foot[0]))
-    else:
-        angle = 0
-    if i == 0:
-        prev_hip_center = hip_center
-        prev_angle = angle
-        i += 1
-        continue
-    else:
-        if descentSpeedCheck(prev_hip_center, hip_center, fps):
-            print(f"Frame {i} is a Fall")
-        if angleCenterlineCheck(prev_angle, angle):
-            print(f"Frame {i} is a Fall")
-  
-    result.save(f'output/frame_{i}.jpg')
+    no_of_people = keypoints.shape[0]
+    for k in range(no_of_people):
+        right_hip = keypoints[k][8]
+        left_hip = keypoints[k][11]
+        hip_center = (right_hip + left_hip) / 2
+        nose = keypoints[k][0]
+        right_foot = keypoints[k][10]
+        left_foot = keypoints[k][13]
+        mean_foot = (right_foot + left_foot) / 2
+        nose = nose.cpu().numpy()
+        mean_foot = mean_foot.cpu().numpy()
+        if nose[0] != mean_foot[0]:
+            angle = np.arctan((nose[1]-mean_foot[1])/(nose[0] - mean_foot[0]))
+        else:
+            angle = 0
+        if i == 0:
+            prev_hip_center = hip_center
+            prev_angle = angle
+            result.save(f'intermediate/frame_{i}.jpg')
+            i += 1
+            continue
+        else:
+            if descentSpeedCheck(prev_hip_center, hip_center, fps):
+                print(f"Frame {i} is a Fall")
+                flag = True
+            if angleCenterlineCheck(prev_angle, angle):
+                print(f"Frame {i} is a Fall")
+                flag = True
+    
+    result.save(f'intermediate/frame_{i}.jpg')
     i += 1
+compileToVideo('./intermediate/','output/output.mp4',flag, height, width, fps)
